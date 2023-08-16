ff56a113728b75bbe451884c4fe54448707edc50
Roberto Simonetti <robisim74@gmail.com>
2023-08-16 22:21:12 +0200

    Update sample

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ COPY requirements.txt /tmp/
 
 RUN pip install --upgrade pip
 RUN pip install --requirement /tmp/requirements.txt
+
+# Required packages
 RUN apt-get update && apt-get install -y \
-  git
+  git \
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
 
+# Version
+print("TensorFlow version: ", tf.__version__)
+# Number of available GPUs
 print("Num GPUs: ", len(tf.config.list_physical_devices('GPU')))
 print(tf.config.list_physical_devices())
+
+# Sample
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
